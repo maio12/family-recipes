ae466823fc932933f230f1a4480d76abc62be818
Antonio_Maiocchi <amaiocchi@red.software.systems>
2021-05-10 22:38:47 +0200

    cleanup server from auth

--- server/app.js
+++ server/app.js
@@ -1,25 +1,17 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const session = require('express-session');
-const uuid = require('uuid/v4');
 const log = require('loglevel');
-const passport = require('passport');
-const MongoStore = require('connect-mongo')(session);
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const expressPlayground = require('graphql-playground-middleware-express')
     .default
 const cors = require('cors');
-// const { ensureAuth, ensureGuest } = require('./middleware/auth');
 
 const app = express();
 
 //allow cross origin requests
 app.use(cors());
-
-//Passport config
-require('./config/passport')(passport)
 
 const options = { 
   useUnifiedTopology: true,
@@ -35,27 +27,12 @@ mongoose.connect(`mongodb+srv://amaiocchi:${process.env.MONGOOSE_PW}@cluster0.zo
   log.warn(`MongoDB connection unsuccessful: ${err}`)
 })
 
-//Sessions middleware
-app.use(session({
-  genid: (req) => uuid(),
-  secret: 'ciao',
-  resave: false,
-  saveUninitialized: false,
-  store: new MongoStore({ mongooseConnection: mongoose.connection })
-}))
-
-//Passport middleware
-app.use(passport.initialize())
-app.use(passport.session())
-
-app.use('/auth', require('./routes/auth'))
 app.use('/graphql', graphqlHTTP({
   schema,
   //graphiql: true,
 }))
 
 app.get('/', expressPlayground({ endpoint: '/graphql' }))
-// app.get('/dashboard')
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => log.warn(`🚀 now listening for requests on port ${port}`));
--- server/server.js
+++
@@ -1,6 +0,0 @@
-const log = require('loglevel');
-const app = require('./app');
-
-const port = process.env.PORT || 4000;
-
-app.listen(port, () => log.info(`🚀 now listening for requests on port ${port}`));
